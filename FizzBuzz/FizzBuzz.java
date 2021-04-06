4d55c92f62ed1d15297b3b724f6550ee9c4bb3f5
James Wyatt <78618659+Arctic-Gif@users.noreply.github.com>
2021-04-05 17:23:16 -0700

    FizzBuzz Method and Tester Java

---
+++ FizzBuzz/FizzBuzz.java
@@ -0,0 +1,19 @@
+public class FizzBuzz {
+    public String fizzBuzz(int number) {
+        if(number == 3){
+            System.out.println("Fizz");
+        }
+        else if(number == 5){
+            System.out.println("Buzz");
+        }
+        else if(number == 15){
+            System.out.println("FizzBuzz");
+        }
+        else if(number == 2){
+            System.out.println(2);
+        }
+        else
+            System.out.println("Nice Try");
+        return("yeah");
+    }
+}
---
+++ FizzBuzz/FizzBuzzTester.java
@@ -0,0 +1,10 @@
+public class FizzBuzzTester{
+    public static void main(String[] args) {
+        FizzBuzz runner = new FizzBuzz();
+        runner.fizzBuzz(3);
+        runner.fizzBuzz(5);
+        runner.fizzBuzz(15);
+        runner.fizzBuzz(2);
+
+    }
+}
